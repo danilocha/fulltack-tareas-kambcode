f9d83bfdababb93ed7911de50bfdc38b0ad73f39
dasan <david.felipe.sanchez2@gmail.com>
2024-04-11 13:35:21 -0500

    tarea2:davidsanchez

---
+++ david-sanchez-tarea2/tarea-2/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import eminem from './images/eminem.jpg';
+
+function App() {
+  return (
+    <div>
+      <h1>Eminem</h1>
+      <img src={eminem} alt="Foto del artista" />
+      <p>
+      Marshall Bruce Mathers III, conocido como Eminem, es un rapero, productor y actor estadounidense.
+       Se le atribuye la popularización del hip hop en las clases medias y altas de,s
+      Estados Unidos y es aclamado por la crítica como uno de los mejores raperos de todos los tiempos
+      </p>
+    </div>
+  );
+}
+
+export default App;
