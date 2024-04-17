748806e1650dc0814894b027fe079c7bf135f1f3
PabloBaez <pablobaez@gmail.com>
2024-04-16 19:27:24 -0500

    tarea2

---
+++ PabloBaez/tarea2/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Armin van Buuren</h1>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrSAMLIDmDYoTQpViRyQvnoT-0QvDo1cV1o2Bo2dmWpA&s"/>
+      <div className="container">Armin Jozef Jacobus Daniël van Buuren es un DJ y productor discográfico neerlandés. 
+      Según el ranking elaborado por DJ Mag, ha sido nombrado DJ número uno del mundo en 2007, 2008, 2009, 2010 y 
+      2012, siendo el único hasta la fecha en conseguir este galardón cuatro veces consecutivas y cinco veces en total.</div>
+    </div>
+  );
+}
+
+export default App;
