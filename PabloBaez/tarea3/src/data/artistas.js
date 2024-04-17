3f9b3cee2e46bab994c1c8af803732c804705b60
PabloBaez <pablobaez@gmail.com>
2024-04-16 20:41:45 -0500

    Tarea3

---
+++ PabloBaez/tarea3/src/data/artistas.js
@@ -0,0 +1,10 @@
+const artistas = [
+  {
+    name: "Armin van Buuren",
+    description: "Armin Jozef Jacobus Daniël van Buuren es un DJ y productor discográfico neerlandés. Según el ranking elaborado por DJ Mag, ha sido nombrado DJ número uno del mundo en 2007, 2008, 2009, 2010 y 2012, siendo el único hasta la fecha en conseguir este galardón cuatro veces consecutivas y cinco veces en total.has four wheels",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrSAMLIDmDYoTQpViRyQvnoT-0QvDo1cV1o2Bo2dmWpA&s",
+  },
+  
+];
+
+export default artistas;
