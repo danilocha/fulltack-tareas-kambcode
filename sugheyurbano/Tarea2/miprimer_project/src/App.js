c09c91a275a7390d92d0a33b9ddcd20b8323d489
Sughey <sughey_urbano@yahoo.es>
2024-04-19 00:58:31 -0500

    Tarea 2 mi Primer React

---
+++ sugheyurbano/Tarea2/miprimer_project/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Marc Anthony</h1>
+      <img src="url-de-la-foto-del-artista" alt="Foto del artista" />
+      <p>
+      Marco Antonio Muñiz Rivera, más conocido como Marc Anthony, es un cantautor 
+      y actor estadounidense de origen puertorriqueño, cuyas canciones van desde 
+      la salsa, pasando por el bolero, la balada y el pop latino.
+      </p>
+    </div>
+  );
+}
+
+export default App;
