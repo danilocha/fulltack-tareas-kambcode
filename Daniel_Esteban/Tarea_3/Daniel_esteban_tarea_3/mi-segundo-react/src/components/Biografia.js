2caa82d0665422120d31c9b462d565133e093ec5
danielestebanll <danielestebanll@icloud.com>
2024-04-22 12:00:37 -0500

    Tareas

---
+++ Daniel_Esteban/Tarea_3/Daniel_esteban_tarea_3/mi-segundo-react/src/components/Biografia.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Biografia() {
+  return (
+    <p>
+      Marshall Bruce Mathers III (St. Joseph, Misuri, 17 de octubre de 1972), conocido como Eminem, 
+      es un rapero, productor y actor estadounidense. Se le atribuye la popularización del hip hop 
+      en las clases medias y altas de Estados Unidos y es aclamado por la crítica como uno de los 
+      mejores raperos de todos los tiempos.1​ Se considera que el éxito mundial y las obras aclamadas 
+      de Eminem rompieron las barreras raciales para la aceptación de los raperos blancos en la música 
+      popular. Si bien gran parte de su trabajo transgresor a fines de la década de 1990 y principios de
+      la de 2000 lo hizo muy controvertido, llegó a ser una «representación» de la angustia popular de 
+      la clase baja estadounidense y ha sido citado como una influencia para muchos artistas de varios géneros.
+    </p>
+  );
+}
+export default Biografia;
