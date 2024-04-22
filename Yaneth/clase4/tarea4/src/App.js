e5b59c700cd505500825be4c9d42e11693482d18
YanethCundarM <yanethcundarmelo@gmail.com>
2024-04-22 13:42:11 -0500

    Tarea4

---
+++ Yaneth/clase4/tarea4/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Reloj from './components/reloj/reloj';
+
+
+function App() {
+  return (
+    <div>
+      <h1>Reloj en Tiempo Real</h1>
+      <Reloj />
+    </div>
+  );
+}
+
+export default App;
