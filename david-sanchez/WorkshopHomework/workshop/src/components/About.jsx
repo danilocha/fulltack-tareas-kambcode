cdcc7c057cc0329e8c95f848b5d8b2747b02b42a
dasan <david.felipe.sanchez2@gmail.com>
2024-04-23 18:49:53 -0500

    workshop-david-sanchez

---
+++ david-sanchez/WorkshopHomework/workshop/src/components/About.jsx
@@ -0,0 +1,8 @@
+const About =()=>{
+    return(
+        <p className="readme">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione ut laudantium itaque labore recusandae magnam saepe nobis iure. Minima
+             consectetur sunt, nostrum voluptatibus maxime laboriosam nulla culpa totam praesentium accusantium.
+        </p>
+    )
+}
+export default About;
---
+++ david-sanchez/WorkshopHomework/workshop/src/components/Readmore.jsx
@@ -0,0 +1,10 @@
+const Readmore = ()=>{
+    return(
+        <p className="boxReadmore">I am characterized by being a professional 
+        with excellent interpersonal relationships, committed to work, capable 
+        of providing improvements and ideas, assuming responsibilities with 
+        adaptability to change and continuous learning.
+        </p>
+    )
+}
+export default Readmore;
