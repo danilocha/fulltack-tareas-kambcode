f9d7981be44306dd439ec1913e6a1ee779c6828a
Sofia Poveda <sofiapoveda@gmail.com>
2024-04-27 21:09:32 -0500

    clase-7-part-2

--- clase-6-workshop/tarea-6-workshop/src/App.js
+++ clase-6-workshop/tarea-6-workshop/src/App.js
@@ -1,26 +1,37 @@
 import React from "react";
-import BasicInfo from "./components/BasicInfo"; // Ruta al archivo donde se define BasicInfo
-import Rendertext from "./components/Rendertext";
-import Workexperience from "./components/Workexperience"; //
-// Define los componentes que deseas mostrar en diferentes rutas.
-// Configura las rutas utilizando el componente Router y Routes:
-
-   
-
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/pages/home";
+import Workexperience from "./components/Workexperience";
+import Projects from "./components/pages/projects";
+import Notfound from "./components/pages/NotFound";
 
 function App() {
   return (
-    <div>
-      <BasicInfo />
-      <Rendertext />
-      <Workexperience />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/workexperience">Work Experience</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />{" "}
+          <Route path="/workexperience" element={<Workexperience />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/#" element={<Notfound />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
-
-
-
 
 export default App;
