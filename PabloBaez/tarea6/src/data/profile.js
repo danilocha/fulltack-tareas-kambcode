e1afde8dbadfcf585cfa8db53e342571092728e1
PabloBaez <pablobaez@gmail.com>
2024-05-05 01:33:43 -0500

    tarea6 portafolio Parte1

---
+++ PabloBaez/tarea6/src/data/profile.js
@@ -0,0 +1,11 @@
+const Profile = [
+  {
+    name: 'Pablo Báez',
+    description: 'Web Developer Javascript Fullstack',
+    image: "https://scontent-bog2-1.xx.fbcdn.net/v/t39.30808-1/440934685_10231824586897534_244321537203229696_n.jpg?stp=dst-jpg_p160x160&_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=OB_F54MAivcQ7kNvgFSX6V8&_nc_ht=scontent-bog2-1.xx&oh=00_AfBzzClCeQsooODnGl1bqZ09zJWIQxBsI2pXtwgMkDs7JA&oe=663A1812",
+    background: "https://www.itprotoday.com/sites/itprotoday.com/files/styles/article_featured_retina/public/JavaScript-code_2.jpg?itok=Zw-BaxPO"
+  }
+  
+];
+
+export default Profile;
