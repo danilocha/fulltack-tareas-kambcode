f78c5d8fc254f1df615f8880145b993d34ca527a
PabloBaez <pablobaez@gmail.com>
2024-05-05 15:33:43 -0500

    Tarea7 Portafolio Parte2

---
+++ PabloBaez/tarea7/src/components/contacto.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './about.module.css';
+
+
+const Contacto = () => {
+    return (
+        <div className={styles.About}>
+        <h2>E-Mail: pablobaez2005@gmail.com</h2>
+        <br />
+        <h2>Whatsapp: +57 315 570 2316</h2>
+        </div>
+    )
+}
+
+export default Contacto;
---
+++ PabloBaez/tarea7/src/pages/inicio/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from '../../components/header';
+import About from '../../components/about';
+import Web from '../../components/web';
+import Navbar from '../../components/menu';
+import '../../App.css';
+
+function Home() {
+  var year = new Date().getFullYear();
+  return (
+    <div className='App'>
+      <Header/>
+      <Navbar/>
+      <About/>
+      <Web/>
+    </div>
+  );
+}
+
+export default Home;
